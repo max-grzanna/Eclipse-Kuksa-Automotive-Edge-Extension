d7765ed1efa3aca292bc33dde3900f3cc882266d
max-grzanna <grzannamax@gmail.com>
2022-06-03 13:29:38 +0200

    #13: updated install script to work with emissary ingress

--- install-kuksa.sh
+++ install-kuksa.sh
@@ -5,33 +5,28 @@ set -o nounset
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-#Install k3s
-printf "Install a specific version of K3s? (Y/n) "
-read -r useVersion
 printf "Install Helm? (Y/n) "
 read -r installHelm
 
-if [[ $useVersion == 'Y' ]]; then
-  # Could be removed, if kuksa-cloud is working properly with latest k8s versions
-  # Default is 1.17.2 because of deprecated CRDs
-  # 1.17.2 is the newest version compatible with kuksa-cloud at the moment
-  printf 'Which version do you want to install? [\x1b[36mLeave blank for v1.17.2+k3s1\x1b[0m]: '
-  read -r version
+printf 'Do you want a custom release name? [\x1b[36mLeave blank for "kuksa-cloud"\x1b[0m]: '
+read -r releaseName
 
-  if [[ -z "$version" ]]; then
-    version="v1.17.2+k3s1"
-  fi
-
-  curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=$version K3S_KUBECONFIG_MODE="644" sh -
-else
-  curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
+if [[ -z "$releaseName" ]]; then
+  releaseName="kuksa-cloud"
 fi
+
+printf 'Custom namespace for kuksa installation? [\x1b[36mLeave blank for "kuksa"\x1b[0m]: '
+read -r namespace
+
+if [[ -z "$namespace" ]]; then
+  namespace="kuksa"
+fi
+
+# installing k3s without traefik (we use emissary ingress instead)
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik" sh -
 
 # Cluster access
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-
-# set permissions
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 
 # Installing Helm
 if [[ $installHelm == 'Y' ]]; then
@@ -41,18 +36,14 @@ if [[ $installHelm == 'Y' ]]; then
   rm get_helm.sh
 fi
 
-# install helm dep
 cd kuksa.cloud/deployment/helm/
 helm dep up kuksa-cloud/
 
-# install charts
-printf 'Do you want a custom release name? [\x1b[36mLeave blank for "kuksa-cloud"\x1b[0m]: '
-read -r releaseName
-
-if [[ -z "$releaseName" ]]; then
-  releaseName="kuksa-cloud"
-fi
-
-helm install "$releaseName" kuksa-cloud/
+# installing charts
+kubectl create namespace "$namespace" &&
+kubectl apply -f https://app.getambassador.io/yaml/emissary/2.2.2/emissary-crds.yaml
+kubectl wait --timeout=90s --for=condition=available deployment emissary-apiext -n emissary-system
+helm install "$releaseName" --namespace "$namespace"  kuksa-cloud/ &&
+kubectl -n "$namespace"  wait --for condition=available --timeout=90s deploy -lapp.kubernetes.io/instance=emissary-ingress
 
 exit
